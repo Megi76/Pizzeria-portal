3f454f55798af78de11905627218d743117925c8
Magdalena Dabrowska <mmdabrowska4@gmail.com>
2020-10-27 20:13:30 +0100

    add material design

--- src/App.js
+++ src/App.js
@@ -7,21 +7,36 @@ import Login from './components/views/Login/Login';
 import Tables from './components/views/Tables/Tables';
 import Waiter from './components/views/Waiter/Waiter';
 import Booking from './components/views/Booking/Booking';
+import { StylesProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {main: '#2B4C6F'},
+    // secondary: {main: '#11cb5f'},
+  },
+});
 
 function App() {
   return (
     <BrowserRouter basename={'/panel'}>
-      <MainLayout>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Kitchen}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Login}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Tables}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Waiter}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/:id`} component={Booking} />
-        </Switch>
-      </MainLayout>
-    </BrowserRouter>  );
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <MainLayout>
+            <Switch>
+              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard}/>
+              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Kitchen}/>
+              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Login}/>
+              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Tables}/>
+              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Waiter}/>
+              <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/:id`} component={Booking} />
+            </Switch>
+          </MainLayout>
+        </ThemeProvider>
+      </StylesProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
